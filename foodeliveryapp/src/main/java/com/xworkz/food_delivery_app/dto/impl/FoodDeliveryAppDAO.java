352712853ab74f55b969f54c9bac30c243f94c62
prateekdeshbhandari <prateekdeshbhandari@gmail.com>
2026-06-16 13:01:15 +0530

    Implemented CRUD operations and validation for Uber details using PreparedStatement

---
+++ foodeliveryapp/src/main/java/com/xworkz/food_delivery_app/dto/impl/FoodDeliveryAppDAO.java
@@ -0,0 +1,4 @@
+package com.xworkz.food_delivery_app.dto.impl;
+
+public class FoodDeliveryAppDAO  {
+}
---
+++ uber-taxi/src/main/java/com/xwork/taxi/servies/TaxiDetielsDtoValidection.java
@@ -0,0 +1,8 @@
+package com.xwork.taxi.servies;
+
+import com.xwork.taxi.dto.TaxiDetailsDTO;
+
+public interface TaxiDetielsDtoValidection {
+    public boolean validetSave(TaxiDetailsDTO taxiDetailsDTO);
+
+}
